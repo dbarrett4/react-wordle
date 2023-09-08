cf17a954b6bb6f2ed2035240a9e1ba946db4b804
Dhillyon Barrett <ed20db@leeds.ac.uk>
2023-09-08 16:22:35 +0100

    Made use of a Merriam-Webster API to check if a word is English. Also extended words.txt.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect} from 'react';
 import GameBoard from './components/GameBoard';
 import WordInput from './components/WordInput';
 import Feedback from './components/Feedback';
+import Streaks from './components/Streaks';
 import './App.css'
 
 const App: React.FC = () => {
@@ -10,11 +11,11 @@ const App: React.FC = () => {
   const[targetWord, setTargetWord] = useState('');
   const[guesses, setGuesses] = useState<string[]>([]);
   const[feedback, setFeedback] = useState<string[]>([]);
-
+  const[streak, setStreak] = useState<number>(0);
+  const[highestStreak, setHighestStreak] = useState<number>(0);
+  const[inputIsDisabled, setInputIsDisabled] = useState<boolean>(false);
 
   const maxAttempts = 6;
-  const streak = 0;
-  const maxStreak = 0;
   
   // Get the list of words from the words.txt file
   const fetchWords = async() => {
@@ -41,10 +42,49 @@ const App: React.FC = () => {
     return words[index];
   };
 
+  // Passed into the WordInput component. Verifys guesses and determines wins/losses
   const handleGuess = async (guess: string) => {
     if (guesses.length >= maxAttempts) {
       // Max attempts reached. Do nothing.
       return;
+    }
+    
+    // Validity checking
+    if (guess.length != 5) {
+      generateFeedback(["Your guess must contain 5 letters"]);
+      return;
+    } else {
+      generateFeedback([""]);
+    }
+    
+    // Check that the word is English against the Merriam-Webster API
+    const apiKey='f030c8b3-7e47-4832-b41f-3aa8a2496776';
+    const apiUrl = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${guess}?key=${apiKey}`;
+
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+
+      if (Array.isArray(data)) {
+        // Check each entry in response
+        const isValidWord = data.some((entry) => {
+          if (entry.hasOwnProperty("meta")) { // Then its a valid word in the dictionary
+            return true;
+          }
+          return false;
+        });
+
+        if (!isValidWord) {
+          generateFeedback(["Your guess must be a valid English word"]);
+          return;
+        }
+        else {
+          generateFeedback([""]);
+        }
+
+      }
+    } catch(error) {
+      console.error('Error validating word: ', error);
     }
     
     // Check if guess is correct
@@ -55,17 +95,30 @@ const App: React.FC = () => {
     
     // Check for win or loss
     if (isCorrect) {
-      generateFeedback(["You've won! The word was: " + targetWord, "Click 'Reset Game' to play again"])
+      setInputIsDisabled(true);
+      generateFeedback(["You've won! The word was: " + targetWord, "Click 'Reset Game' to play again"]);
+
+      // Set streaks
+      if ((streak + 1) > highestStreak) {
+        setHighestStreak(streak + 1);
+      }
+      setStreak(streak + 1);
+      
     }
     else if (guesses.length >= maxAttempts - 1) {
-      generateFeedback(["You've lost. The word was: " + targetWord, "Click 'Reset Game' to play again"])
+      setInputIsDisabled(true);
+      generateFeedback(["You've lost. The word was: " + targetWord, "Click 'Reset Game' to play again"]);
+      setStreak(0);
     }
     
   };
 
-  
   // Resets game board to initial state and selects a new word
   const resetGame = () => {
+    if (!inputIsDisabled) {
+      setStreak(0);
+    }
+    setInputIsDisabled(false);
     setGuesses([]);
     setFeedback(["Guess the 5 letter word"]);
     setTargetWord(generateRandomWord());
@@ -83,9 +136,10 @@ const App: React.FC = () => {
   return (
     <div className='App'>
       <h1>React Wordle</h1>
+      <Streaks currentStreak={streak} highestStreak={highestStreak}></Streaks>
       <Feedback feedback={feedback}></Feedback>
       <GameBoard targetWord={targetWord} guesses={guesses} />
-      <WordInput onGuess={handleGuess}/>
+      <WordInput onGuess={handleGuess} disabled={inputIsDisabled}/> 
       <br></br>
       <button onClick={resetGame} className='btn btn-primary'>Reset Game</button>
     </div>
--- src/components/WordInput.tsx
+++ src/components/WordInput.tsx
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 
 interface WordInputProps {
     onGuess: (guess: string) => void;
+    disabled: boolean
 }
 
-const WordInput: React.FC<WordInputProps> = ({ onGuess }) => {
+const WordInput: React.FC<WordInputProps> = ({ onGuess, disabled }) => {
     const [inputValue, setInputValue] = useState('');
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -14,10 +15,6 @@ const WordInput: React.FC<WordInputProps> = ({ onGuess }) => {
     const handleFormSubmit = (event: React.FormEvent) => {
         event.preventDefault();
 
-        if (inputValue.length != 5) {
-            alert('Word must be 5 characters')
-            return;
-        }
         
         // If input is valid, call onGuess function with guess
         if (inputValue.trim() !== '') {
@@ -38,6 +35,7 @@ const WordInput: React.FC<WordInputProps> = ({ onGuess }) => {
                                 placeholder='Enter your guess...'
                                 value={inputValue}
                                 onChange={handleInputChange}
+                                disabled={disabled}
                                 />
                         </div>
                         <button type='submit' className='btn btn-primary'>
